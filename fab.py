c655d4ba79dad84f072b48c863e87ee52aefd761
kk <kk@gmail.com>
2022-12-08 06:05:06 +0000

    python file

--- fab.py
+++ fab.py
@@ -6,7 +6,7 @@ def recur_fibo(n):
    else:
        return(recur_fibo(n-1) + recur_fibo(n-2))
 
-nterms = 10
+nterms = int(input("Enter How much Fibonacci Want \n"))
 
 # check if the number of terms is valid
 if nterms <= 0:
